863ceafd4ca9cd80b4c4291e0130bddffe0c6961
Danil_ch <DChubiryaev@bellintegrator.com>
2020-05-05 11:02:01 +0300

    Should be commit with Objectify connection   =)

--- Api/api/src/main/java/com/dog/api/controllers/FormController.java
+++ Api/api/src/main/java/com/dog/api/controllers/FormController.java
@@ -1,5 +1,6 @@
 package com.dog.api.controllers;
 
+import com.dog.api.persistance.dao.UserFormDAO;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,7 +8,9 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-public class Controller {
+public class FormController {
+
+    private UserFormDAO userFormDAO;
 
     @GetMapping(path = "/")
     public String hello() {
@@ -17,6 +20,8 @@ public class Controller {
     @PostMapping(path = "/save", produces = MediaType.APPLICATION_JSON_VALUE)
     public UserFormResponse saveUserForm(@RequestBody UserForm form) {
         System.out.println("POST DATA " + form.toString());
+//        Key<UserForm> userFormKey = userFormDAO.saveAndReturnKey(form);
+//        System.out.println(userFormKey.getId());
         return new UserFormResponse("Response!", 1);
     }
 
